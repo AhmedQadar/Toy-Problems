808461638eb3051fc6b0a34362973af96b7113db
ahmedqadar <abdulnet029@gmail.com>
2023-10-06 12:07:13 +0300

    test push

---
+++ speedDetector.js
@@ -0,0 +1,10 @@
+function speedDetector (speed) {
+    if (speed <= 70) {
+       console.log('Okay');
+    } else {
+        let points = (speed - 70) /5 ;
+        if (points > 12) {
+            console.log('License Suspended');
+        }
+    }
+    
